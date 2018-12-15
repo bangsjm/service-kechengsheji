26df46d308b4412bb82ac5a4941af384fe03a036
0 error <18765758978@163.com>
2018-12-15 18:01:14 +0800

    学生管理

---
+++ src/main/java/com.youngc.pipeline/service/Student/SearchScoreService.java
@@ -0,0 +1,4 @@
+package com.youngc.pipeline.service.Student;
+
+public interface SearchScoreService {
+}
---
+++ src/main/java/com.youngc.pipeline/service/Student/impl/SearchScoreServiceImpl.java
@@ -0,0 +1,6 @@
+package com.youngc.pipeline.service.Student.impl;
+
+import com.youngc.pipeline.service.Student.SearchScoreService;
+
+public class SearchScoreServiceImpl implements SearchScoreService {
+}
